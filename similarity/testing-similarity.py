ae79b6017c3f70299be0f01588c9014bc8d77b0c
Rahul Markasserithodi <rahul4nair35@gmail.com>
2025-04-19 22:57:40 +1000

    similarity testing

--- similarity/testing-similarity.py
+++ similarity/testing-similarity.py
@@ -5,7 +5,7 @@ def read_json(file_path):
     with open(file_path, 'r', encoding="utf-8") as f:
         return json.load(f)
 
-tests = read_json('dataset/alan-similarity-evals.json')
+tests = read_json('dataset/similarity-evals.json')
 
 for i, test in enumerate(tests):
     actual_score = get_similarity(test['prompt1'], test['prompt2'])
