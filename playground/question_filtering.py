8ad2a3b414c87df8a373c8a33687c447c2a0cd3c
Ishman Singh <ishmanbirsingh@gmail.com>
2025-04-24 23:45:55 +1000

    Added datasets and fixed judge

--- playground/question_filtering.py
+++ playground/question_filtering.py
@@ -1,9 +1,7 @@
 from transformers import pipeline
 from datasets import load_dataset
-from tqdm import tqdm
 
-classifier = pipeline("zero-shot-classification",
-                      model="facebook/bart-large-mnli")
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 candidate_labels = ['This is a question', 'This is a declaration', 'This is a prompt']
 
 def is_question(prompt):
@@ -15,23 +13,28 @@ def is_question(prompt):
     else:
         return 0
 
+# Load Datasets
+train_dataset = load_dataset("PKU-Alignment/BeaverTails", split="30k_train")
+train_dataset = train_dataset.filter(lambda x: not x["is_safe"])
 
-dataset = load_dataset("PKU-Alignment/BeaverTails", split="30k_train")
-dataset = dataset.filter(lambda x: not x["is_safe"])
-# smaller = dataset.select(range(150))
+test_dataset = load_dataset("PKU-Alignment/BeaverTails", split="30k_test")
+test_dataset = test_dataset.filter(lambda x: not x["is_safe"])
 
-filtered_dataset = dataset.filter(lambda x: is_question(x["prompt"]))
-filtered_dataset.to_csv("../dataset/filtered_dataset.csv", index=False)
+# Filter Datasets
+filtered_train_dataset = train_dataset.filter(lambda x: is_question(x["prompt"]))
+filtered_test_dataset = test_dataset.filter(lambda x: is_question(x["prompt"]))
 
+# Split Datasets
+train_split = filtered_train_dataset.train_test_split(train_size = 1000, test_size = 30, seed = 0)
+final_train_dataset = train_split["train"]
+final_validation_dataset = train_split["test"]
 
+test_split = filtered_test_dataset.train_test_split(train_size = 100, test_size = 100, seed = 0)
+final_test_dataset = test_split["train"]
+final_reward_eval_dataset = test_split["test"]
 
-# # Load the CSV dataset
-# test_dataset = load_dataset("csv", data_files="../dataset/test_filtering.csv")
-# for sample in tqdm(test_dataset["train"]):
-#     prompt = sample["prompt"]
-#     answer = sample["is_question"]
-#     if is_question(prompt) != answer:
-#         print(prompt)
-#         classification = classifier(prompt, candidate_labels, multi_label=True)
-#         print(classification)
-#         print()
+# Save Datasets
+final_train_dataset.to_csv("../dataset/train_dataset.csv", index=False)
+final_validation_dataset.to_csv("../dataset/validation_dataset.csv", index=False)
+final_test_dataset.to_csv("../dataset/test_dataset.csv", index=False)
+final_reward_eval_dataset.to_csv("../dataset/reward_eval_dataset.csv", index=False)
